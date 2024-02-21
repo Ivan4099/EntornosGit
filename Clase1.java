15cfc377ab68465f9231f8b025f934ab6a27de67
Ivan <pepe19722004@gmail.com>
2024-02-21 09:44:28 +0100

    tengo

--- Clase1.java
+++ Clase1.java
@@ -5,7 +5,7 @@ public class Clase1 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Adios mundo");
-
+		System.out.println("Tengo hambre");
 	}
 
 }
--- Clase2.java
+++ Clase2.java
@@ -5,6 +5,7 @@ public class Clase2 {
 	public static void main(String[] args) {
 		System.out.println("Patata");
 		System.out.println("Jojos es GOD");
+		System.out.println("Tengo sueño");
 	}
 
 }
