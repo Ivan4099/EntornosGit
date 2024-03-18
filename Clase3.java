9722b86fcbc24bb0428d950f834387fbb7c5dafc
Ivan4099 <159776225+Ivan4099@users.noreply.github.com>
2024-03-18 10:11:11 +0100

    Contador
    Contador

--- Clase3.java
+++ Clase3.java
@@ -3,6 +3,8 @@ package entornosGit;
 public class Clase3 {
 
 	public static void main(String[] args) {
+		int contador;
+		contador=0;
 		System.out.println("Hola profe");
 		System.out.println("No me suspendas");
 		System.out.println("Porfa");
