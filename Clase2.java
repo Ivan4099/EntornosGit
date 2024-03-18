8154b4be544298113ceef1e105f88ef55278275b
Ivan4099 <159776225+Ivan4099@users.noreply.github.com>
2024-03-18 10:09:47 +0100

    Escribir hola
    Escribir hola

--- Clase2.java
+++ Clase2.java
@@ -5,6 +5,7 @@ public class Clase2 {
 	public static void main(String[] args) {
 		String hola;
 		hola="hola";
+		System.out.println(hola);
 		System.out.println("Patata");
 		System.out.println("Jojos es GOD");
 		System.out.println("Tengo sueño");
